f5ab30fe4f3e017e6e3a791b29b72ad3f8f6957e
Vunguyennhauyen <nhauyen70205@gmail.com>
2025-11-17 22:37:45 +0700

    Update analyticsRoutes.js

--- Backend/analytics-service/routes/analyticsRoutes.js
+++ Backend/analytics-service/routes/analyticsRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { getReports } from "../controllers/analyticsController.js";
+import {
+  getFailureRate,
+  analyzeRootCause,
+  forecastCost
+} from "../controllers/analytics.controller.js";
 
 const router = express.Router();
 
-router.get("/reports", getReports);
+router.get("/failure-rate", getFailureRate);
+router.get("/root-cause", analyzeRootCause);
+router.get("/forecast", forecastCost);
 
 export default router;
-
