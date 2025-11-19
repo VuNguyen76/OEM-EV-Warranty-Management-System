b5d85d6911f4f8569f915bf947095d45226de94c
Tinh516725 <tinhnq212@gmail.com>
2025-11-19 19:09:19 +0700

    update docker config

--- Backend/api-gateway/index.js
+++ Backend/api-gateway/index.js
@@ -6,7 +6,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(morgan("dev"));
 
 const services = {
